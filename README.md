b37c2efcd35deb27e535ba923e87bd8275ceeef4
wangteng <wangteng@cnnic.cn>
2024-08-29 17:04:45 +0800

    update

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ yarn dev
 ## 学习资料
 
 <https://developer.mozilla.org/zh-CN/docs/Web/CSS/CSS_grid_layout>
+[echarts配置](https://juejin.cn/post/7223641276850241593)
--- src/components/china.vue
+++ src/components/china.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="china-chart">
-    <tiny-chart-map
-      style="width: 1000px"
-      :data="chartData"
-      :settings="chartSettings"
-    ></tiny-chart-map>
+    <tiny-chart-map width="100vh" height="100vh" :data="chartData" :settings="chartSettings"></tiny-chart-map>
   </div>
 </template>
 
 <script setup lang="jsx">
-  import { ref, getCurrentInstance } from "vue";
-  import { ChartMap as TinyChartMap } from "@opentiny/vue";
-  import chinaData from "../assets/china.json";
+import { ref, getCurrentInstance } from "vue";
+import { ChartMap as TinyChartMap } from "@opentiny/vue";
+import chinaData from "../assets/china.json";
 
-  const chartData = ref({
-    columns: ["位置", "GDP"],
-    rows: [
-      { 位置: "吉林省", GDP: 123 },
-      { 位置: "北京市", GDP: 1223 },
-      { 位置: "上海省", GDP: 2123 },
-      { 位置: "浙江省", GDP: 4123 },
-    ],
-  });
-  const chartSettings = ref({
-    position: "china",
-    mapOrigin: chinaData, // 用户自己的地图数据
-    dataType: {
-      GDP: "KMB",
-    },
-  });
+const chartData = ref({
+  columns: ["位置", "GDP"],
+  rows: [
+    { 位置: "吉林省", GDP: 123 },
+    { 位置: "北京市", GDP: 1223 },
+    { 位置: "上海省", GDP: 2123 },
+    { 位置: "浙江省", GDP: 4123 },
+  ],
+});
+const chartSettings = ref({
+  position: "china",
+  mapOrigin: chinaData, // 用户自己的地图数据
+  dataType: {
+    GDP: "KMB",
+  },
+
+});
 </script>
 
 <style scoped></style>
