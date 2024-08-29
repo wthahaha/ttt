f7f873d3202bdf2d04c9617640e288a63f0dfce2
cncert <635870838@qq.com>
2024-08-30 00:17:06 +0800

    update

--- src/components/china.vue
+++ src/components/china.vue
@@ -1,31 +1,69 @@
 <template>
   <div class="china-chart">
-    <tiny-chart-map width="100vh" height="100vh" :data="chartData" :settings="chartSettings"></tiny-chart-map>
+    <tiny-chart-map
+      width="100vh"
+      height="100vh"
+      :data="chartData"
+      :settings="chartSettings"
+    ></tiny-chart-map>
   </div>
 </template>
 
 <script setup lang="jsx">
-import { ref, getCurrentInstance } from "vue";
-import { ChartMap as TinyChartMap } from "@opentiny/vue";
-import chinaData from "../assets/china.json";
+  import { ref, getCurrentInstance } from "vue";
+  import { ChartMap as TinyChartMap } from "@opentiny/vue";
+  import chinaData from "../assets/china.json";
 
-const chartData = ref({
-  columns: ["位置", "GDP"],
-  rows: [
-    { 位置: "吉林省", GDP: 123 },
-    { 位置: "北京市", GDP: 1223 },
-    { 位置: "上海省", GDP: 2123 },
-    { 位置: "浙江省", GDP: 4123 },
-  ],
-});
-const chartSettings = ref({
-  position: "china",
-  mapOrigin: chinaData, // 用户自己的地图数据
-  dataType: {
-    GDP: "KMB",
-  },
-
-});
+  const chartData = ref({
+    columns: ["位置", "GDP"],
+    rows: [
+      { 位置: "吉林省", GDP: 123 },
+      { 位置: "北京市", GDP: 1223 },
+      { 位置: "上海省", GDP: 2123 },
+      { 位置: "浙江省", GDP: 4123 },
+    ],
+  });
+  const chartSettings = ref({
+    position: "china",
+    mapOrigin: chinaData, // 用户自己的地图数据
+    dataType: {
+      GDP: "KMB",
+    },
+    zoom: 1.2,
+    show: true,
+    roam: false,
+    emphasis: {
+      label: {
+        show: false,
+      },
+    },
+    label: {
+      show: false,
+    },
+    layoutSize: "100%",
+    itemStyle: {
+      normal: {
+        areaColor: "#013c72", // 区域颜色
+        borderColor: "#00c3ff", // 边界线颜色
+        borderWidth: 1,
+        shadowColor: "green",
+        shadowOffsetX: 0,
+        shadowOffsetY: 5,
+        shadowBlur: 1,
+      },
+    },
+    visualMap: {
+      min: 0,
+      max: 1000,
+      left: "left",
+      top: "bottom",
+      text: ["高", "低"], // 文本，默认为数值文本
+      calculable: true,
+      inRange: {
+        color: ["#1f3b73", "#00c3ff"], // 色带范围
+      },
+    },
+  });
 </script>
 
 <style scoped></style>
--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -20,110 +20,110 @@
       <China />
     </div>
   </div>
-
 </template>
 
 <script setup lang="jsx">
-import { ref } from "vue";
-import China from "./china.vue";
+  import { ref } from "vue";
+  import China from "./china.vue";
 </script>
 
 <style scoped>
-.container {
-  position: relative;
-  /* 相对定位容器 */
-  display: grid;
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100vh;
-  /* 设置容器高度为屏幕高度 */
-  background-color: lightblue;
-  grid-template-areas: "full-screen";
-}
+  .container {
+    position: relative;
+    /* 相对定位容器 */
+    display: grid;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    /* 设置容器高度为屏幕高度 */
+    background-color: lightblue;
+    grid-template-areas: "full-screen";
+  }
 
-.full-screen {
-  display: grid;
-  margin: 0;
-  padding: 0;
-  grid-template-columns: repeat(6,
-      1fr);
-  /* 将页面分成6列，每列宽度是1等份，也就是总宽度/6,总宽度不需要指定 */
-  height: 100vh;
-  /* 设置容器高度为屏幕高度，需要指定 */
-  grid-gap: 5px;
-  grid-template-rows: repeat(4,
-      calc(25vh - 5px));
-  /* 将页面分成4行，每行高度是(总高度/4)-行间距 */
-  z-index: 1;
-}
+  .full-screen {
+    display: grid;
+    margin: 0;
+    padding: 0;
+    grid-template-columns: repeat(6, 1fr);
+    /* 将页面分成6列，每列宽度是1等份，也就是总宽度/6,总宽度不需要指定 */
+    height: 100vh;
+    /* 设置容器高度为屏幕高度，需要指定 */
+    grid-gap: 5px;
+    grid-template-rows: repeat(4, calc(25vh - 5px));
+    /* 将页面分成4行，每行高度是(总高度/4)-行间距 */
+    z-index: 1;
+  }
 
-.map {
-  position: absolute;
-  /* 绝对定位 */
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url('../images/bg.jpg');
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  .map {
+    position: absolute;
+    /* 绝对定位 */
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("../images/bg.jpg");
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
 
-.one {
-  grid-column: 1 / 2;
-  /*设置每个区块开始和结束的位置 开始 / 结束 的位置 */
-  grid-row: 1;
-}
+  .one {
+    grid-column: 1 / 2;
+    /*设置每个区块开始和结束的位置 开始 / 结束 的位置 */
+    grid-row: 1;
+  }
 
-.two {
-  grid-column: 6;
-  grid-row: 1;
-}
+  .two {
+    grid-column: 6;
+    grid-row: 1;
+  }
 
-.three {
-  grid-row: 2 / 4;
-  grid-column: 1;
-}
+  .three {
+    grid-row: 2 / 4;
+    grid-column: 1;
+  }
 
-.four {
-  grid-row: 2 / 4;
-  grid-column: 6;
-}
+  .four {
+    grid-row: 2 / 4;
+    grid-column: 6;
+  }
 
-.five {
-  grid-row: 4;
-  grid-column: 1;
-}
+  .five {
+    grid-row: 4;
+    grid-column: 1;
+  }
 
-.six {
-  grid-row: 4;
-  grid-column: 6;
-}
+  .six {
+    grid-row: 4;
+    grid-column: 6;
+  }
 
-.seven {
-  grid-row: 4;
-  grid-column: 2 / 6;
-  display: grid;
-  margin: 0;
-  padding: 0;
-  grid-template-columns: repeat(6, 1fr);
-  grid-gap: 5px;
-  grid-template-rows: repeat(1, calc(25vh - 5px));
-}
+  .seven {
+    grid-row: 4;
+    grid-column: 2 / 6;
+    display: grid;
+    margin: 0;
+    padding: 0;
+    grid-template-columns: repeat(6, 1fr);
+    grid-gap: 5px;
+    grid-template-rows: repeat(1, calc(25vh - 5px));
+  }
 
-.seven>div {
-  background-color: rgb(170, 183, 183, 0.2);
-}
+  .seven > div {
+    background-color: rgb(170, 183, 183, 0.2);
+  }
 
-.one,
-.two,
-.three,
-.four,
-.five,
-.six,
-.seven {
-  background-color: rgba(116, 85, 85, 0.2);
-}
+  .one,
+  .two,
+  .three,
+  .four,
+  .five,
+  .six,
+  .seven {
+    background-color: rgba(116, 85, 85, 0.2);
+  }
 </style>
