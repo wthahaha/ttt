07e830c112615bfef601001c6864e573c3c88c0f
wangteng <wangteng@cnnic.cn>
2024-08-30 17:14:21 +0800

    update

--- src/components/china.vue
+++ src/components/china.vue
@@ -11,20 +11,11 @@ import { ChartMap as TinyChartMap } from "@opentiny/vue";
 import chinaData from "../assets/china.json";
 
 const chartData = ref({
-  columns: ["位置", "GDP"],
-  rows: [
-    { 位置: "吉林省", GDP: 123 },
-    { 位置: "北京市", GDP: 1223 },
-    { 位置: "上海省", GDP: 2123 },
-    { 位置: "浙江省", GDP: 4123 },
-  ],
+  columns: ["名称", "数据"],
 });
 const chartSettings = ref({
   position: "china",
   mapOrigin: chinaData, // 用户自己的地图数据
-  dataType: {
-    GDP: "KMB",
-  },
   zoom: 1.8,
   center: [104.114129, 33.550339], // 设置地图的中心点
   show: true,
@@ -57,9 +48,50 @@ const chartSettings = ref({
     text: ["高", "低"], // 文本，默认为数值文本
     calculable: true,
     inRange: {
-      color: ["#1f3b73", "#00c3ff"], // 色带范围
+      color: ["#1f3b73", "#00c3ff"], // 有数据的省份颜色
     },
-  }
+    outOfRange: {
+      color: "#e0e0e0", // 没有数据的省份颜色
+      label: { show: false }, // 没有数据的省份不显示标签
+    },
+
+  },
+  series: [{
+    type: 'map',
+    data: [
+      { name: '吉林省', value: 123 },
+      { name: '北京市', value: 1223 },
+      { name: '上海市', value: 2123 },
+      { name: '浙江省', value: 114123 },
+    ],
+    markPoint: {
+      symbol: 'pin', // 自定义标记图形
+      symbolSize: 50, // 标记大小
+      label: {
+        show: true,  // 显示标签
+        formatter: '{b}\n{c}', // 格式为：名称 + 换行符 + 数值
+        color: '#000', // 标签文字颜色
+        backgroundColor: '#fff', // 标签背景颜色
+        borderColor: '#000', // 标签边框颜色
+        borderWidth: 1, // 标签边框宽度
+        borderRadius: 3, // 标签圆角半径
+        padding: [4, 6], // 标签内边距（上右下左）
+        shadowColor: 'rgba(0, 0, 0, 0.2)', // 标签阴影颜色
+        shadowBlur: 10, // 标签阴影模糊
+        shadowOffsetX: 2, // 标签阴影横向偏移
+        shadowOffsetY: 2, // 标签阴影纵向偏移
+      },
+      itemStyle: {
+        color: '#f40', // 标记颜色
+      },
+      data: [
+        { name: '吉林省', coord: [125.325990, 43.897016], value: 123 },
+        { name: '北京市', coord: [116.407526, 39.90403], value: 1223 },
+        { name: '上海市', coord: [121.473701, 31.230416], value: 2123 },
+        { name: '浙江省', coord: [120.153576, 30.287459], value: 114123 }
+      ]
+    }
+  }]
 });
 </script>
 
---
+++ src/components/world.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-chart ref="map" :option="option" autoresize />
+</template>
+
+<script setup>
+import { use, registerMap } from "echarts/core";
+import { ScatterChart, EffectScatterChart } from "echarts/charts";
+import VChart, { THEME_KEY } from "vue-echarts";
+import {
+  GeoComponent,
+  TitleComponent,
+  LegendComponent,
+  TooltipComponent
+} from "echarts/components";
+import { shallowRef } from "vue";
+import getData from "../data/map";
+import chinaMap from "../assets/china.json";
+
+use([
+  ScatterChart,
+  EffectScatterChart,
+  GeoComponent,
+  TitleComponent,
+  LegendComponent,
+  TooltipComponent
+]);
+
+registerMap("china", chinaMap);
+
+const option = shallowRef(getData());
+const map = shallowRef(null);
+</script>
